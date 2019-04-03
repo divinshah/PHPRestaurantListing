c4893e31c3300cef282ce9f57d896ae616146a52
divyashah9724330544 <divyashah9724330544@gmail.com>
2019-04-03 13:26:58 -0400

    image upload

--- Divyalisting/Gallery/header.php
+++ Divyalisting/Gallery/header.php
@@ -9,7 +9,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
     
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/HFstyle.css">
         
       
 <body>
