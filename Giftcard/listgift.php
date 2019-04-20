42a8da7ba53e0705add2e5d7d455609016fb459d
ridham passey <ridham.passey@gmail.com>
2019-04-19 23:17:13 -0400

    giftcard

--- Giftcard/listgift.php
+++ Giftcard/listgift.php
@@ -4,21 +4,21 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 <?php
-require_once 'database.php';
+require_once '../model/database.php';
 require_once 'giftcard.php';
 
 $dbcon = Database::getDb();
 $s = new giftcard();
 $mygift =  $s->getGIFTCARD(Database::getDb());
 foreach($mygift as $giftcard){
-    echo "<li><a href='detailgift.php?id=$giftcard->id'>" .  $giftcard->Email  . "</a>".
+    echo "<li><a href='detailgift.php?id=$giftcard->ID'>" .  $giftcard->email  . "</a>".
 	    "<div class='container'>".
         "<form action='editgift.php' method='post'>" .
-        "<input type='hidden' class='form-control' value='$giftcard->id' name='id' />".
+        "<input type='hidden' class='form-control' value='$giftcard->ID' name='id' />".
         "<input type='submit' class='form-control' value='Update' name='update' />".
         "</form>" .
         "<form action='deletegift.php' method='post'>" .
-        "<input type='hidden' class='form-control' value='$giftcard->id' name='id' />".
+        "<input type='hidden' class='form-control' value='$giftcard->ID' name='id' />".
         "<input type='submit' class='form-control' value='Delete' name='delete' />".
         "</form>".
 		"</div>".
